23e247df4a5ffdafef36696cd816d9552ca3a8ef
Martin Knoller <linuxswords@gmail.com>
2025-12-21 11:51:23 +0100

    migrate junit test 4 -> 5

--- app/src/test/java/org/linuxswords/games/chessclock/ExampleUnitTest.java
+++ app/src/test/java/org/linuxswords/games/chessclock/ExampleUnitTest.java
@@ -1,15 +1,11 @@
 package org.linuxswords.games.chessclock;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest
+class ExampleUnitTest
 {
 
 }
